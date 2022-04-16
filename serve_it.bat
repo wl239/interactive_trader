169b16a881d9bd0c02294176011efb04d8c61323
wl239 <wl239@duke.edu>
2022-04-16 19:25:17 -0400

    Update serve bat

--- serve_it.bat
+++ serve_it.bat
@@ -1,4 +1,4 @@
 cd C:\Users\wl239\PycharmProjects\interactive_trader
-git pull https://%TESTAPP_GIT_PAT%@github.com/wl239/homework_3-1.git
+git pull https://%TESTAPP_GIT_PAT%@github.com/wl239/interactive_trader.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
